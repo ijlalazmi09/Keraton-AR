bda36a5c8c0443168f123634d4b7f570a903c369
Nur-beginner <eranew262@gmail.com>
2025-09-03 07:28:31 +0800

    perapihan penulisan

--- Keraton Project/script.js
+++ Keraton Project/script.js
@@ -1,23 +1,13 @@
-        document.addEventListener('DOMContentLoaded', function() {
-            const cards = document.querySelectorAll('.card');
-            
-            cards.forEach((card, index) => {
-                card.style.animationDelay = `${index * 0.2}s`;
-                card.classList.add('fade-in');
-            });
+gsap.from("h2", {
+  opacity: 0,
+  duration: 1,
+  x: -30,
+  delay: 1,
+});
 
-        });
-
-        gsap.from("h2",{
-                opacity:0,
-                duration:1,
-                x:-30,
-                delay:1
-        })
-
-        gsap.from("p",{
-            opacity:0,
-            duration:1,
-            y:30,
-            delay:1
-        })
+gsap.from("p", {
+  opacity: 0,
+  duration: 1,
+  y: 30,
+  delay: 1,
+});
