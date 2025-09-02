6a7d02035ad7adf3b941abad612eb5c32821dda8
ijlalazmi09 <ijlalazmi09@gmail.com>
2025-09-02 15:39:54 +0800

    script.js

--- Keraton Project/script.js
+++ Keraton Project/script.js
@@ -5,4 +5,19 @@
                 card.style.animationDelay = `${index * 0.2}s`;
                 card.classList.add('fade-in');
             });
+
         });
+
+        gsap.from("h2",{
+                opacity:0,
+                duration:1,
+                x:-30,
+                delay:1
+        })
+
+        gsap.from("p",{
+            opacity:0,
+            duration:1,
+            y:30,
+            delay:1
+        })
